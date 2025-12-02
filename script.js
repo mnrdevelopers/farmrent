e04b7ee93a919e1ef14ae7bf4975b730e5e1aa84
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-12-02 10:34:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-const storage = firebase.storage();
 
 // Global variables
 let currentUser = null;
