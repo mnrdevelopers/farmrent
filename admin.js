74c37b5fd7e8e60be11207dd5863e147df8b1cea
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-12-08 18:27:15 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1688,7 +1688,7 @@ function getCategoryIcon(categoryName) {
         'jcb': 'fas fa-truck-pickup',
         'grass-cutter': 'fas fa-cut',
         'trolley': 'fas fa-truck-moving',
-        'water-tanker': 'fa-truck-loading',
+        'water-tanker': 'fas fa-truck',
         'default': 'fas fa-tools'
     };
     
